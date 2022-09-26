2307cc0995fe959e54842a7714a18146c68efd0c
connorswanson1 <connorswanson13@gmail.com>
2022-09-25 22:20:56 -0600

    init picture field fix

--- 05/SuperheroDatabase/hero/migrations/0001_initial.py
+++ 05/SuperheroDatabase/hero/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200)),
                 ('body', models.TextField()),
-                ('picture', models.ImageField(upload_to='.static/images')),
+                ('picture', models.CharField(max_length=200)),
             ],
         ),
     ]
