d0c21626e8f8a8870bfa818e5b69902319311bc9
connorswanson1 <connorswanson13@gmail.com>
2022-09-25 21:50:51 -0600

    change in image format

--- 05/SuperheroDatabase/hero/models.py
+++ 05/SuperheroDatabase/hero/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Hero(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
-    picture = models.ImageField(upload_to='./static/images')
+    picture = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.pk}. {self.name}'
