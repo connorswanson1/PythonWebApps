b3dec746d2294826582ce095114094e6657069f8
connorswanson1 <connorswanson13@gmail.com>
2022-09-25 20:18:37 -0600

    admin page updates

--- 05/SuperheroDatabase/hero/models.py
+++ 05/SuperheroDatabase/hero/models.py
@@ -5,3 +5,6 @@ class Hero(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
     picture = models.ImageField(upload_to='.static/images')
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}'
