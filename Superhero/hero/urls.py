5e768601f8d1ad0769498c656ac88825e4b5ae82
connorswanson1 <connorswanson13@gmail.com>
2022-10-07 14:37:15 -0600

    added profile redirect

--- Superhero/hero/urls.py
+++ Superhero/hero/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView, SignUpView
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
@@ -10,12 +11,13 @@ urlpatterns = [
     path('add',             HeroCreateView.as_view(),  name='hero_add'),
     path('<int:pk>/',       HeroUpdateView.as_view(),  name='hero_edit'),
     path('<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
-    
-    #Accounts
+
+    # Accounts
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
-    
-    #Admin
+    path('accounts/profile/', RedirectView.as_view(url='../..')),
+
+    # Admin
     path('admin/', admin.site.urls),
 
 ]
