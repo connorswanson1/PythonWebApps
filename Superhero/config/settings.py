2f988000ef5c944d40579e0ff5584b058781839e
connorswanson1 <connorswanson13@gmail.com>
2022-11-04 15:44:12 -0600

    Database fix, command insert

--- Superhero/config/settings.py
+++ Superhero/config/settings.py
@@ -78,10 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": environ.get("DATABASE_ENGINE"),
-        "NAME": environ.get("DATABASE_NAME"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
